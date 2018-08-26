1885e3899bf9da15ac52152c39bbe253a9b4a61c
Sasha Hart <sashanicolehart@gmail.com>
2018-08-26 02:13:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musiciansArray, instrumentsArray) {
+  var list = [];
+  var i = 0;
+  for ( let i=0; i <= musiciansArray.length - 1; i++ ) {
+    list.push(`${musiciansArray[i]} plays ${instrumentsArray[i]}`);
+  }
+  return list;
+}
+
+function johnLennonFacts(array) {
+  let i = array.length - 1;
+  while (i >= 0) {
+    array[i] = `${array[i]}!!!`
+    i--;
+  }
+  return array;
+}
